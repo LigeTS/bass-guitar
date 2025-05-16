a6da08623f157b9e62090c38321e38a8a8bc2e59
LigeTS <ligetsmith@gmail.com>
2025-05-16 08:37:30 -0500

    go

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import BassGuitar from './components/BassGuitar';
 import './App.css';
 
-<<<<<<< HEAD
 function App() {
   return (
     <div className="App">
@@ -13,14 +12,3 @@ function App() {
 
 export default App;
 
-=======
-function App() {
-  return (
-    <div className="App">
-      <BassGuitar />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 70099eea49db549b8cca995b499c3b0ccbc55743
