a0e36b1f415d591a8ba644ba3c73ba1c235a9b58
LigeTS <ligetsmith@gmail.com>
2025-05-16 08:36:37 -0500

    fixed it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BassGuitar from './components/BassGuitar';
 import './App.css';
 
+<<<<<<< HEAD
 function App() {
   return (
     <div className="App">
@@ -11,3 +12,15 @@ function App() {
 }
 
 export default App;
+
+=======
+function App() {
+  return (
+    <div className="App">
+      <BassGuitar />
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 70099eea49db549b8cca995b499c3b0ccbc55743
