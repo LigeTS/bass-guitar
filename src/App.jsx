70099eea49db549b8cca995b499c3b0ccbc55743
“LTSmith” <ligetsmith@gmail.com>
2025-05-16 08:15:30 -0500

    Reset App.jsx with clean implementation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BassGuitar from './components/BassGuitar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BassGuitar />
+    </div>
+  );
+}
+
+export default App;
